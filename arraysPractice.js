ff1e7cd644a63e9f1b8a5e6bacfb9048329177ff
Jeff Chapman <jeffcchapman@gmail.com>
2017-01-15 18:40:45 -0700

    finished finder()

--- arraysPractice.js
+++ arraysPractice.js
@@ -106,15 +106,28 @@ divider(numbersArray);
 var getRandomArbitrary = function() {
   return Math.floor(Math.random() * (30 - 0) + 0);
 };
-// var numbers = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
+var numbers = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 //Above you're given a function that will return a random number between 0 and 30.  There is also a commented out array full of numbers to help you visualize what your function will be receiving.
 
 // Your job is to write a function named finder that will get a random number (by invoking getRandomArbitrary), then loop through the array (that will be passed in as a parameter) to see if that random number is in the array. If it is, return true, if it's not, return false
 
   //Code Here
+function finder(arr) {
+  var getRandomArbitrary = function() {
+    return Math.floor(Math.random() * (30 - 0) + 0);
+  };
+  var numCheck = getRandomArbitrary();
+  console.log(numCheck);
+  var response = false;
+  for (var i = 0; i<arr.length; i++){
+    if (arr[i] === numCheck) {
+      response = true;
+    }
+  }
+  return response;
+}
 
-  //Code Here
-
+finder(numbers);
 
 //Next problem
 
